9ee9b090fcec58d6fbe73fa20d2ed0d5cfd0a096
guibiaoguo <guoa1234@163.com>
2022-03-16 05:04:40 +0800

    update istore

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,14 +11,14 @@
 #
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 git clone https://github.com/linkease/istore.git package/istore
-
+git clone https://github.com/linkease/istore.git package/istore-ui
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
 
