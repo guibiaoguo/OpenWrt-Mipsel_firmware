bdece7127d73608c2d23fa0eacf0edaa7755972b
guibiaoguo <guoa1234@163.com>
2022-03-17 07:28:58 +0800

    update theme edge istore bypass ikoolproxy 多线多拨

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,16 +12,14 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kiddin9/openwrt-bypass.git package/bypass
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/linkease/istore-ui.git package/istore-ui
-git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+
 # 删除自定义源默认的 argon 主题
 rm -rf package/lean/luci-theme-argon
 
 # 拉取 argon 原作者的源码
-git clone https://github.com/kiddin9/luci-theme-edge.git package/lean/luci-theme-edge
+git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/lean/luci-theme-edge
+
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 替换默认主题为 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-edge/' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
