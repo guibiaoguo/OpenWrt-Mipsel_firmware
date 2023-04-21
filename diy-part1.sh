24e65def753c6cc7dfc1e6684998943b57cc0f55
guibiaoguo <guoa1234@163.com>
2023-04-21 08:47:14 +0800

    add all

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,8 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
