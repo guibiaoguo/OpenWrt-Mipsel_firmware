9f47a3f28ceb7fc3d8bb5fc8f6b93152428fa9b3
guibiaoguo <guoa1234@163.com>
2023-04-22 04:22:52 +0800

    update samba kernel

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+rm -rf package/luci-theme-argon
+git clone clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
