8618ff5d58e148184c0822792eefff0d980b0707
guibiaoguo <guoa1234@163.com>
2023-04-22 04:44:53 +0800

    update samba kernel app-vssr

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,3 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-#git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
-rm -rf package/luci-theme-argon
-git clone clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
