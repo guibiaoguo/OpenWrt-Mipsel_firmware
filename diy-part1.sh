c1b59630fde03272fa12f22487539c5c4023ef51
guibiaoguo <guoa1234@163.com>
2023-04-22 17:25:28 +0800

    add  helloworld

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@ git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/luci-t
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-# sed -i "/helloworld/d" "feeds.conf.default"
-# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
