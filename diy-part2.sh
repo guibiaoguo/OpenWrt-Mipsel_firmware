831b95ec4bf25e74cda422fb0d319ae0c93da3a1
guibiaoguo <guoa1234@163.com>
2023-04-22 09:22:32 +0800

    update ext4 ntfs-3g samba

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+rm -rf package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
