6c3c144edefdd0360627a0cdfb12b506a5d1732d
guibiaoguo <guoa1234@163.com>
2023-04-24 05:11:21 +0800

    update M

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 rm -rf package/feeds/luci/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/packages/aliyundrive-webdav/
+rm -rf package/feeds/luci/luci-app-aliyundrive-webdav
+cp -rf feeds/kenzo/aliyundrive-webdav/ package/feeds/packages/aliyundrive-webdav/
+cp -rf feeds/kenzo/luci-app-aliyundrive-webdav/ package/feeds/luci/luci-app-aliyundrive-webdav/
