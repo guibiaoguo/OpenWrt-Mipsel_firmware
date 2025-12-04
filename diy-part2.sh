2a794a3230f71c1965b7f7988b7a0e92866bc77e
guibiaoguo <guoa1234@163.com>
2025-12-05 05:19:23 +0800

    fix: 添加v2arya

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 rm -rf package/feeds/luci/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
 #rm -rf package/feeds/packages/aliyundrive-webdav/
 #rm -rf package/feeds/luci/luci-app-aliyundrive-webdav
 #cp -rf feeds/kenzo/aliyundrive-webdav/ package/feeds/packages/aliyundrive-webdav/
